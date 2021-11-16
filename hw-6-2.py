4b47221bba4b42b6fd3cc75bb4c67648ba2eafd4
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-11-15 19:45:47 -0500

    Add file

---
+++ hw-6-2.py
@@ -0,0 +1,31 @@
+# Author: JRI 11/15/2021
+
+# Question 1
+x = [1,2,3,4,5,6,7]
+
+# Question 2
+print(len(x))
+
+# Question 3
+print(x[2])
+
+# Question 4
+print(not(5 in x))
+
+# Question 5
+print(not(10 in x))
+
+# Question 6
+x2 = ['Cam', 'Timmy', 'Jack', 'Ryan']
+
+# Question 7
+print(x2[0])
+
+# Question 8
+print(x2 * 3)
+
+# Question 9
+x2.extend(x2)
+
+# Question 10
+print(len(x))
